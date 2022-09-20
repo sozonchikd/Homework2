0b465c97e19c2f39568c4aa56530d3936d41e00d
Sozonchik <den.sozon@mail.ru>
2022-09-20 14:51:44 +0500

    Сделано ДЗ 2

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Задача 1
+        System.out.println(" задача 1");
         var dog = 8.0;
         System.out.println(dog);
         var cat = 3.6;
         System.out.println(cat);
         var paper = 763789;
         System.out.println(paper);
+
+        // Задача 2
+        System.out.println(" задача 2");
 dog = dog + 4;
 System.out.println(dog);
 cat = cat + 4;
 System.out.println(cat);
 paper = paper + 4;
 System.out.println(paper);
+
+        System.out.println(" задача 3");
 dog = dog - 3.5;
 System.out.println(dog);
 cat = cat - 1.6;
@@ -20,12 +27,15 @@ System.out.println(cat);
 paper = paper - 7639;
 System.out.println(paper);
 
+        System.out.println(" задача 4");
 var friend = 19;
 System.out.println(friend);
 friend = friend + 2;
 System.out.println(friend);
 friend = friend / 7;
 System.out.println(friend);
+
+        System.out.println(" задача 5");
 var frog = 3.5;
 System.out.println(frog);
 frog = frog * 10;
@@ -35,19 +45,24 @@ System.out.println(frog);
 frog = frog + 4;
 System.out.println(frog);
 
+        System.out.println(" задача 6");
 var weightBoxer1 = 78.2;
 var weightBoxer2 = 82.7;
 var weighTotalBoxer = weightBoxer1 + weightBoxer2;
 System.out.println("Общий вес бойцов " + weighTotalBoxer + " кг");
 var weightDifference = weightBoxer2 - weightBoxer1;
 System.out.println("Разница веса между бойцами составляет " + weightDifference + " кг");
-var weightDifference2 = weightBoxer2 % weightBoxer1;
-System.out.println("Разница веса между бойцами составляет " + weightDifference2 + " кг");
 
+        System.out.println(" задача 7");
+var weightDifference2 = weightBoxer2 % weightBoxer1;
+System.out.println("(2 способ) Разница веса между бойцами составляет " + weightDifference2 + " кг");
+
+        System.out.println(" задача 8");
 var totalHours = 640;
 var workingTime = 8;
 var totalWorkers = totalHours / workingTime;
 System.out.println("Всего работников в компании – " + totalWorkers + " человек");
+
 var newWorkers = totalWorkers + 94;
 var totalHoursNewWorkers = newWorkers * workingTime;
 System.out.println("Если в компании работает " + newWorkers+ " человека, то всего " + totalHoursNewWorkers + " часа работы может быть поделено между сотрудниками");
