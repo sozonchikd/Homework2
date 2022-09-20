68648b05948ec4a8fca2d3819722b15afed3d394
Sozonchik <den.sozon@mail.ru>
2022-09-20 12:35:42 +0500

    задание 2 выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        var dog = 8;
+        var dog = 8.0;
         System.out.println(dog);
         var cat = 3.6;
         System.out.println(cat);
@@ -13,5 +13,26 @@ cat = cat + 4;
 System.out.println(cat);
 paper = paper + 4;
 System.out.println(paper);
+dog = dog - 3.5;
+System.out.println(dog);
+cat = cat - 1.6;
+System.out.println(cat);
+paper = paper - 7639;
+System.out.println(paper);
+
+var friend = 19;
+System.out.println(friend);
+friend = friend + 2;
+System.out.println(friend);
+friend = friend / 7;
+System.out.println(friend);
+var frog = 3.5;
+System.out.println(frog);
+frog = frog * 10;
+System.out.println(frog);
+frog = frog / 3.5;
+System.out.println(frog);
+frog = frog + 4;
+System.out.println(frog);
     }
 }
